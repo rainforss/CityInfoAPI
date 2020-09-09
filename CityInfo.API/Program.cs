88e46517574362e7a7bb8912049000e24f951fa4
rainforss <shengyan@ualberta.ca>
2020-09-08 22:15:09 -0600

    EF core implemented

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using CityInfo.API.Contexts;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace CityInfo.API
@@ -14,7 +17,17 @@ namespace CityInfo.API
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host=CreateWebHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetService<CityInfoContext>();
+                context.Database.EnsureDeleted();
+                context.Database.Migrate();
+            }
+
+            host.Run();
+
+
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
